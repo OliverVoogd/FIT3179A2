ac89eb2a3d8ceb9eb639db87d8a1676f77bd9ff1
Oliver Voogd <51314988+OliverVoogd@users.noreply.github.com>
2021-10-16 21:29:55 +1100

    line and layout

--- js/embed_graphs.js
+++ js/embed_graphs.js
@@ -1,6 +1,7 @@
 var vg_1 = "js/global_disasters_map.vg.json"
 var vg_2 = "js/select_countries_disasters.vg.json"
-
+var vg_3 = "js/disasters_deaths.vg.json"
+var vg_4 = "js/line_chart.vg.json"
 
 vegaEmbed("#global_map", vg_1, {"actions": false}).then(function(result) {
 
@@ -9,3 +10,11 @@ vegaEmbed("#global_map", vg_1, {"actions": false}).then(function(result) {
 vegaEmbed("#select_countries", vg_2, {"actions": false}).then(function(result) {
 
 }).catch(console.error) 
+
+vegaEmbed("#disasters_deaths", vg_3, {"actions": false}).then(function(result) {
+
+}).catch(console.error)
+
+vegaEmbed("#line_chart", vg_4, {"actions": false}).then(function(result) {
+
+}).catch(console.error)
