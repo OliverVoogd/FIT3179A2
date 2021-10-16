62f1034aa4e13c69f4259dbb99222195b58554a3
Oliver Voogd <51314988+OliverVoogd@users.noreply.github.com>
2021-10-16 11:00:48 +1100

    updated dates

--- data/edit_dates.py
+++ data/edit_dates.py
@@ -4,20 +4,27 @@ i = cols.index("Start Year")
 print(i)
 new = open("g:\My Drive\_UNI\FIT3179\A2\data\emdat_trimmed_joined_sedac_datefixed.csv", "w")
 new.write(",".join(cols[:i]))
-new.write(",date")
+new.write(",date,")
 new.write(",".join(cols[i+3:]))
 
 
 for line in old:
-	line = line.split(",")
-	print(line)
-	date = line[i+2] + "/" + line[i+1] + "/" + line[i]
+	try:
+		line = line.split(",")
+		if (line[i+2] == "0"):
+			line[i+2] = "1"
+		if (line[i+1] == "0"):
+			line[i+1] = "1"
+			
+		date = line[i+2] + "/" + line[i+1] + "/" + line[i]
 
-	for c in range(i):
-		new.write(line[c])
+		for c in range(i):
+			new.write(line[c])
+			new.write(",")
+
+		new.write(date)
 		new.write(",")
 
-	new.write(date)
-	new.write(",")
-
-	new.write(",".join(line[i+3:]))
+		new.write(",".join(line[i+3:]))
+	except:
+		continue
